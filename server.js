974ce9cb3dd9ba03f7482c2e9f791a5d6338e2bd
Paul-Louis NECH <pln@algolia.com>
2017-08-16 17:21:24 +0200

    server: Search artist with algolia

--- server.js
+++ server.js
@@ -1,8 +1,47 @@
 const Hapi = require('hapi');
 const Good = require('good');
+const algoliasearch = require('algoliasearch');
+const client = algoliasearch("TDNMRH8LS3", "ec222292c9b89b658fe00b34ff341194");
+const index = client.initIndex("songs");
 
-const handleFulfilment = function (request, reply) {
-    reply("Payload:" + request.payload);
+const handleFulfilment = function (req, reply) {
+    const TAG = "fulfil";
+    let output = '';
+
+    let artist = '';
+    if (req.payload.result.parameters['artist']) {
+        artist = req.payload.result.parameters['artist'];
+        server.log(TAG, "Artist: " + artist);
+    }
+
+    let period = '';
+    if (req.payload.result.parameters['period']) {
+        period = req.payload.result.parameters['period'];
+        server.log(TAG, "Period: " + period);
+    }
+
+    if (artist.length !== 0)
+    {
+        let songs = [];
+        index.search(artist, (err, content) => {
+            if (err) {
+                server.error(err);
+                return;
+            }
+
+            server.log(TAG, "Searching for " + artist + " returned " + content.nbHits + " results.");
+            for (let i in content.hits) {
+                let hit = content.hits[i];
+                server.log(TAG, 'Hit(' + hit.objectID + '): ' + hit.trackName);
+                songs.push(hit.trackName);
+            }
+            output += "hits: " + songs.join(", ") + ".";
+            server.log(TAG, "output:" + output);
+            reply(output);
+        });
+    } else {
+        server.log(TAG, "No artist.");
+    }
 };
 
 // Init server
