65c66a5444b4a6d8cd6f68d5f8f9b84f13195b28
Paul-Louis NECH <pln@algolia.com>
2017-08-16 15:53:19 +0200

    server: logging

--- server.js
+++ server.js
@@ -1,24 +1,50 @@
 const Hapi = require('hapi');
+const Good = require('good');
 
-get = function (request, reply) {
-    reply("Success!\n");
-};
-
+// Init server
 const server = new Hapi.Server();
 server.connection({
     host: +process.env.HOST || '0.0.0.0',
     port: +process.env.PORT || 8000
 });
+
+// Setup routes
 server.route({
     method: 'GET',
     path: '/',
-    handler: get
+    handler: function (request, reply) {
+        reply("Success!\n");
+    }
 });
 
-server.start((err) => {
-   if (err) {
-       throw err;
-   }
+// Setup reporting
+server.register({
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                    response: '*',
+                    log: '*'
+                }]
+            }, {
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+}, (err) => {
+    if (err) {
+        throw err; // error while loading the plugin
+    }
+});
 
-   console.log("Server running at ", server.info.uri);
+// Run server
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log("Server running at ", server.info.uri);
 });
