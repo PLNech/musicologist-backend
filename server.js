5d1f9cbf4feec76283d39e30b65e83541f1fd117
Paul-Louis NECH <pln@algolia.com>
2017-08-23 15:20:33 +0200

    fulfilment: handling period, sendReply helper

--- server.js
+++ server.js
@@ -8,16 +8,28 @@ const version = 5;
 const handleFulfilment = function (req, reply) {
     const TAG = "fulfil";
     let artist = '', artistOriginal = '', period = '';
-    let artistNames = [];
+    let period_start = 0, period_end = 0;
+    let artistNames = [], songs = [];
     let response = {
         'source': "Algolia",
         'backend_version': version,
         data: []
     };
 
+    let sendReply = function (message, code) {
+        if (message != undefined) {
+            response["speech"] = message;
+            response["displayText"] = message;
+        }
+        if (code == undefined) {
+            code = 200;
+        }
+        server.log(TAG, "Reply(" + code + "):" + response["speech"]);
+        reply(response).code(code);
+    };
+
     if (!(req.mime && req.mime == "application/json")) {
-        response["speech"] = "Your request MUST be application/json.";
-        reply(response).code(400);
+        sendReply("Your request MUST be application/json.", 400);
         return;
     }
 
@@ -29,58 +41,73 @@ const handleFulfilment = function (req, reply) {
     if (req.payload.result.parameters['period']) {
         period = req.payload.result.parameters['period'];
         server.log(TAG, "Period: " + period);
+        [period_start, period_end] = period.split("/").map(Date.parse).map(x => x / 1000 /* engine expects second-level precision */);
+        server.log(TAG, "Split: " + period_start + " to " + period_end + " -> " + new Date(period_start) + " to " + new Date(period_end));
     }
 
 
-    if (artist.length !== 0) { // Search for the given artist
-        let songs = [];
-        index.search(artist, {restrictSearchableAttributes: ['artistName']}, (err, content) => {
-                if (err) {
-                    server.error(err);
-                    return;
-                }
+    let searchQuery = '';
+    const searchOptions = {};
+    if (artist != '') {
+        searchOptions['restrictSearchableAttributes'] = ['artistName'];
+        searchQuery = artist;
+    }
 
-                server.log(TAG, "Searching for " + artist + " returned " + content.nbHits + " results.");
-                if (content.nbHits > 0) {
-                    for (let i in content.hits) {
-                        let hit = content.hits[i];
-                        if (artistNames.indexOf(hit.artistName) == -1) {
-                            artistNames.push(hit.artistName);
-                        }
-                        server.log(TAG, 'Hit(' + hit.objectID + '): ' + hit.trackName);
-                        songs.push(hit);
-                    }
-                    response["data"] = {"songs": songs};
-                    const artistIsFoundExact = artistNames.indexOf(artist) != -1;
+    if (period != '') {
+        const filter = "release_timestamp: " + period_start + ' TO ' + period_end;
+        server.log(TAG, "filter: " + filter);
+        searchOptions['filters'] = filter;
+    }
 
-                    if (artistNames.length == 1) {
-                        if (artistIsFoundExact) { // We found the expected artist
-                            response["speech"] = "I found those songs by " + artistNames[0] + ": " + songs.map(hit => hit.trackName).join(", ") + ".";
-                        } else { // We found another artist -> trigger OTHER_ARTIST event
-                            response["followupEvent"] = {name: "OTHER_ARTIST"};
-                            response["contextOut"] = [{
-                                name: "otherArtist",
-                                parameters: {
-                                    'artistOriginal': artistOriginal,
-                                    'artistActual': artistNames[0]
-                                },
-                                lifespan: 2
-                            }];
-                            delete response.data
-                        }
-                    } else { // We found several artists -> reply with the input
-                        response["speech"] = "I found those songs from several artists matching \"" + artistOriginal + "\": " + songs.map(hit => hit.trackName).join(", ") + ".";
-                        response["data"]["artists"] = artistNames;
-                    }
-                } else {
-                    response["speech"] = "I'm afraid I know no songs from " + artist + ".";
-                }
-                server.log(TAG, "speech:" + response["speech"]);
-                response["displayText"] = response["speech"];
-                reply(response);
+    if (artist == '' && period == '') {
+        server.log(TAG, "No artist nor period -> nothing to search.");
+        sendReply("I can't search for nothing. Please ask me about music by an artist or from a date/perion!");
+        return;
+    }
+
+    index.search(searchQuery, searchOptions, (err, content) => {
+            if (err) {
+                server.log(err);
+                return;
             }
-        );
-    }
+
+            server.log(TAG, "Searching for " + artist + " returned " + content.nbHits + " results.");
+            if (content.nbHits > 0) {
+                for (let i in content.hits) {
+                    let hit = content.hits[i];
+                    if (artistNames.indexOf(hit.artistName) == -1) {
+                        artistNames.push(hit.artistName);
+                    }
+                    server.log(TAG, 'Hit(' + hit.objectID + '): ' + hit.trackName);
+                    songs.push(hit);
+                }
+                response["data"] = {"songs": songs};
+                const artistIsFoundExact = artistNames.indexOf(artist) != -1;
+
+                if (artistNames.length == 1) {
+                    if (artistIsFoundExact) { // We found the expected artist
+                        response["speech"] = "I found those songs by " + artistNames[0] + ": " + songs.map(hit => hit.trackName).join(", ") + ".";
+                    } else { // We found another artist -> trigger OTHER_ARTIST event
+                        response["contextOut"] = [{
+                            name: "otherArtist",
+                            parameters: {
+                                'artistOriginal': artistOriginal,
+                                'artistActual': artistNames[0]
+                            },
+                            lifespan: 2
+                        }];
+                        delete response.data
+                    }
+                } else { // We found several artists -> reply with the input
+                    response["speech"] = "I found those songs from several artists matching \"" + artistOriginal + "\": " + songs.map(hit => hit.trackName).join(", ") + ".";
+                    response["data"]["artists"] = artistNames;
+                }
+            } else {
+                response["speech"] = "I'm afraid I know no songs by " + artist + ".";
+            }
+            sendReply();
+        }
+    );
 };
 
 // Init server
